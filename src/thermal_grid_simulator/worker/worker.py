2c1cdd0227176a78284c7a902cf826f54e046f11
Shapagat <shapagatbolat9@gmail.com>
2026-04-26 12:04:16 +0200

    refactor: add entry point | isolate DB write

--- src/thermal_grid_simulator/worker/worker.py
+++ src/thermal_grid_simulator/worker/worker.py
@@ -11,39 +11,49 @@ MAX_RETRIES = 3
 
 
 def process_message(data):
-    db = SessionLocal()
+    db_data = build_device_data(data)
+    save_to_db(db_data)
 
-    db_data = DeviceData(
+
+def build_device_data(data):
+    return DeviceData(
         device_id=data["device_id"],
         temperature=data["temperature"],
         energy_usage=data["energy_usage"],
         timestamp=data["timestamp"]
     )
 
+def save_to_db(db_data):
+    db = SessionLocal()
     db.add(db_data)
     db.commit()
     db.close()
 
-while True:
-    message = redis_client.brpop("device_queue")
-    raw_data = message[1]
-    data = json.loads(raw_data)
 
-    retries = data.get("retries", 0)
+def run_worker():
+    while True:
+        message = redis_client.brpop("device_queue")
+        raw_data = message[1]
+        data = json.loads(raw_data)
 
-    try:
-        logging.info(f"Processing device {data['device_id']}")
-        process_message(data)
+        retries = data.get("retries", 0)
 
-    except Exception as e:
-        logging.error(f"Error: {e}")
+        try:
+            logging.info(f"Processing device {data['device_id']}")
+            process_message(data)
 
-        if retries < MAX_RETRIES:
-            time.sleep(1)
-            data["retries"] = retries + 1
-            redis_client.lpush("device_queue", json.dumps(data))
-            logging.info(f"Retrying ({data['retries']})")
+        except Exception as e:
+            logging.error(f"Error: {e}")
 
-        else:
-            redis_client.lpush("dead_letter_queue", json.dumps(data))
-            logging.error("Moved to dead letter queue")
+            if retries < MAX_RETRIES:
+                time.sleep(1)
+                data["retries"] = retries + 1
+                redis_client.lpush("device_queue", json.dumps(data))
+                logging.info(f"Retrying ({data['retries']})")
+
+            else:
+                redis_client.lpush("dead_letter_queue", json.dumps(data))
+                logging.error("Moved to dead letter queue")
+
+if __name__ == "__main__":
+    run_worker()
